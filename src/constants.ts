6fe32f7ad32236e313ee0e5bb6fc8991738dc389
Romain Durand <romain.durand@radiofrance.com>
2024-11-23 20:05:23 +0100

    define blousque/skeet constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,46 +1,20 @@
 import { Label } from './types.js';
 
-export const DELETE = 'insert-rkey-of-delete-post-here';
-export const LABEL_LIMIT = 1;
+export const DELETE = '3lbn6d5edzc2u';
+export const LABEL_LIMIT = 2;
 export const LABELS: Label[] = [
   {
-    rkey: 'insert-rkey-here',
-    identifier: 'earth',
+    rkey: '3lbn6dunxfc2u',
+    identifier: 'blousque',
     locales: [
-      { lang: 'en', name: 'Earth 🌎', description: 'Earth'},
-      { lang: 'pt-BR', name: 'Terra 🌎', description: 'Terra'},
+      { lang: 'en', name: 'blousque', description: 'blousque'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
-    identifier: 'fire',
+    rkey: '3lbn6lm7oj22u',
+    identifier: 'skeet',
     locales: [
-      { lang: 'en', name: 'Fire 🔥', description: 'Fire'},
-      { lang: 'pt-BR', name: 'Fogo 🔥', description: 'Fogo'},
-    ]
-  },
-  {
-    rkey: 'insert-rkey-here',
-    identifier: 'air',
-    locales: [
-      { lang: 'en', name: 'Air 💨', description: 'Air'},
-      { lang: 'pt-BR', name: 'Ar 💨', description: 'Ar'},
-    ]
-  },
-  {
-    rkey: 'insert-rkey-here',
-    identifier: 'water',
-    locales: [
-      { lang: 'en', name: 'Water 💧', description: 'Water'},
-      { lang: 'pt-BR', name: 'Água 💧', description: 'Água'},
-    ]
-  },
-  {
-    rkey: 'insert-rkey-here',
-    identifier: 'love',
-    locales: [
-      { lang: 'en', name: 'Love 💞', description: 'Love'},
-      { lang: 'pt-BR', name: 'Amor 💞', description: 'Amor'},
+      { lang: 'en', name: 'skeet', description: 'skeet'},
     ]
   },
 ];
