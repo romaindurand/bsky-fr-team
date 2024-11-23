269abb5dd1f9a94c94edc95c79b3db8c57d22a6f
Romain Durand <romaindurand89@gmail.com>
2024-11-23 23:12:52 +0100

    add post and rkeys

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 import { Label } from './types.js';
 
 export const DELETE = '3lbn6d5edzc2u';
-export const LABEL_LIMIT = 2;
+export const LABEL_LIMIT = 5;
 export const LABELS: Label[] = [
   {
     rkey: '3lbn6dunxfc2u',
@@ -18,17 +18,24 @@ export const LABELS: Label[] = [
     ]
   },
   {
-    rkey: '',
+    rkey: '3lbnbghdxrs2h',
     identifier: 'tweet',
     locales: [
       { lang: 'en', name: 'tweet', description: "Cette personne utilise le mot tweet pour désigner ce qu'elle envoie sur Bluesky"},
     ]
   },
   {
-    rkey: '',
+    rkey: '3lbnbjqwpvk2g',
     identifier: 'schtroumpf',
     locales: [
       { lang: 'en', name: 'schtroumpf', description: "Cette personne utilise le mot schtroumpf pour désigner ce qu'elle envoie sur Bluesky"},
+    ]
+  },
+  {
+    rkey: '3lbnhwx5xdc2l',
+    identifier: 'post',
+    locales: [
+      { lang: 'en', name: 'post', description: "Cette personne utilise le mot post pour désigner ce qu'elle envoie sur Bluesky"},
     ]
   },
 ];
