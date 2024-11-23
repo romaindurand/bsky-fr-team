b0949b6b1813f423dcd97414bf42c73fc50a1ae8
Romain Durand <romain.durand@radiofrance.com>
2024-11-23 20:46:39 +0100

    add tweet + schtroumpf

--- src/constants.ts
+++ src/constants.ts
@@ -7,14 +7,28 @@ export const LABELS: Label[] = [
     rkey: '3lbn6dunxfc2u',
     identifier: 'blousque',
     locales: [
-      { lang: 'en', name: 'blousque', description: 'blousque'},
+      { lang: 'en', name: 'blousque', description: "Cette personne utilise le mot blousque pour désigner ce qu'elle envoie sur Bluesky"},
     ]
   },
   {
     rkey: '3lbn6lm7oj22u',
     identifier: 'skeet',
     locales: [
-      { lang: 'en', name: 'skeet', description: 'skeet'},
+      { lang: 'en', name: 'skeet', description: "Cette personne utilise le mot skeet pour désigner ce qu'elle envoie sur Bluesky"},
+    ]
+  },
+  {
+    rkey: '',
+    identifier: 'tweet',
+    locales: [
+      { lang: 'en', name: 'tweet', description: "Cette personne utilise le mot tweet pour désigner ce qu'elle envoie sur Bluesky"},
+    ]
+  },
+  {
+    rkey: '',
+    identifier: 'schtroumpf',
+    locales: [
+      { lang: 'en', name: 'schtroumpf', description: "Cette personne utilise le mot schtroumpf pour désigner ce qu'elle envoie sur Bluesky"},
     ]
   },
 ];
