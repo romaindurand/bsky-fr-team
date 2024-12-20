3556a8d974c0f4d0b99bd1d3fe6a2387176e92a1
Romain Durand <romaindurand89@gmail.com>
2024-12-20 07:03:11 +0100

    add bleet + blousk

--- src/constants.ts
+++ src/constants.ts
@@ -7,35 +7,49 @@ export const LABELS: Label[] = [
     rkey: '3lbn6dunxfc2u',
     identifier: 'blousque',
     locales: [
-      { lang: 'en', name: 'blousque', description: "Cette personne utilise le mot blousque pour désigner ce qu'elle envoie sur Bluesky"},
+      { lang: 'en', name: 'blousque', description: "Cette personne utilise le mot blousque pour désigner les publications sur Bluesky"},
     ]
   },
   {
     rkey: '3lbn6lm7oj22u',
     identifier: 'skeet',
     locales: [
-      { lang: 'en', name: 'skeet', description: "Cette personne utilise le mot skeet pour désigner ce qu'elle envoie sur Bluesky"},
+      { lang: 'en', name: 'skeet', description: "Cette personne utilise le mot skeet pour désigner les publications sur Bluesky"},
     ]
   },
   {
     rkey: '3lbnbghdxrs2h',
     identifier: 'tweet',
     locales: [
-      { lang: 'en', name: 'tweet', description: "Cette personne utilise le mot tweet pour désigner ce qu'elle envoie sur Bluesky"},
+      { lang: 'en', name: 'tweet', description: "Cette personne utilise le mot tweet pour désigner les publications sur Bluesky"},
     ]
   },
   {
     rkey: '3lbnbjqwpvk2g',
     identifier: 'schtroumpf',
     locales: [
-      { lang: 'en', name: 'schtroumpf', description: "Cette personne utilise le mot schtroumpf pour désigner ce qu'elle envoie sur Bluesky"},
+      { lang: 'en', name: 'schtroumpf', description: "Cette personne utilise le mot schtroumpf pour désigner les publications sur Bluesky"},
     ]
   },
   {
     rkey: '3lbnhwx5xdc2l',
     identifier: 'post',
     locales: [
-      { lang: 'en', name: 'post', description: "Cette personne utilise le mot post pour désigner ce qu'elle envoie sur Bluesky"},
+      { lang: 'en', name: 'post', description: "Cette personne utilise le mot post pour désigner les publications sur Bluesky"},
+    ]
+  },
+  {
+    rkey: '3lcq6imbdnk2e',
+    identifier: 'bleet',
+    locales: [
+      { lang: 'en', name: 'bleet', description: "Cette personne utilise le mot bleet pour désigner les publications sur Bluesky"},
+    ]
+  },
+  {
+    rkey: '3lbtesslmpc2u',
+    identifier: 'blousk',
+    locales: [
+      { lang: 'en', name: 'blousk', description: "Cette personne utilise le mot blousk pour désigner les publications sur Bluesky"},
     ]
   },
 ];
